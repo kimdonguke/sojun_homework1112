0c65a20ad5249fc02a09938c585213ff0fa63978
kimdonguke <rlaehddnr100@gmail.com>
2019-11-13 02:06:52 +0900

    gimoddi

--- app/src/main/java/com/example/sojun_homework1112/Data.java
+++
@@ -1,5 +0,0 @@
-package com.example.sojun_homework1112;
-
-public class Data {
-    String id,pas;
-}
--- app/src/main/java/com/example/sojun_homework1112/ResisterActivity.java
+++ app/src/main/java/com/example/sojun_homework1112/ResisterActivity.java
@@ -2,8 +2,14 @@ package com.example.sojun_homework1112;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class ResisterActivity extends AppCompatActivity {
+    ArrayList<String> res_idlist;
+    ArrayList<String> res_paslist;
+    EditText rid,rpas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
