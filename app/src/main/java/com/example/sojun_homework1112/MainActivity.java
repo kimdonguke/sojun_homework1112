cf8a7e81084140f9975d7354b84fbdc92738cdf8
kimdonguke <rlaehddnr100@gmail.com>
2019-11-13 22:38:05 +0900

    store Arraylist to sharedpreference

--- app/src/main/java/com/example/sojun_homework1112/MainActivity.java
+++ app/src/main/java/com/example/sojun_homework1112/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.sojun_homework1112;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 
@@ -16,11 +22,15 @@ public class MainActivity extends AppCompatActivity {
     Button btn,loginbtn;
     ArrayList<String> idlist;
     ArrayList<String> paslist;
+    Intent intent;
+    int chid,chpas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Intent intent=new Intent(this, ResisterActivity.class);
+        idlist=getStringArrayPref(getApplicationContext(),"id");
+        paslist=getStringArrayPref(getApplicationContext(),"pas");
+       intent=new Intent(this, ResisterActivity.class);
         id=findViewById(R.id.Login_id);
         pas=findViewById(R.id.Login_pas);
         btn=findViewById(R.id.resister_btn);
@@ -36,9 +46,49 @@ public class MainActivity extends AppCompatActivity {
            public void onClick(View v) {
                idstr=id.getText().toString();
                passtr=pas.getText().toString();
-               Toast.makeText(MainActivity.this, "이건 버그야", Toast.LENGTH_SHORT).show();
+               if(idlist.contains(idstr)){
+                   chid=1;
+               }
+               else{
+                   chid=0;
+               }
+               if(paslist.contains(passtr)){
+                   chpas=1;
+               }
+               else{
+                   chpas=0;
+               }
+               if(chid==0&&chpas==0){
+                   Toast.makeText(MainActivity.this, "id 와 password 모두 틀렷습니다", Toast.LENGTH_SHORT).show();
+               }
+               else if(chid==1&&chpas==0){
+                   Toast.makeText(MainActivity.this, "password 가 틀렸습니다", Toast.LENGTH_SHORT).show();
+               }
+               else if(chid==0&&chpas==1){
+                   Toast.makeText(MainActivity.this, "일치하는 id가 없습니다", Toast.LENGTH_SHORT).show();
+               }
+               else{
+
+               }
+
            }
        });
-
+    }
+    private ArrayList<String> getStringArrayPref(Context context, String key) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String json = prefs.getString(key, null);
+        ArrayList<String> urls = new ArrayList<String>();
+        if (json != null) {
+            try {
+                JSONArray a = new JSONArray(json);
+                for (int i = 0; i < a.length(); i++) {
+                    String url = a.optString(i);
+                    urls.add(url);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return urls;
     }
 }
--- app/src/main/java/com/example/sojun_homework1112/ResisterActivity.java
+++ app/src/main/java/com/example/sojun_homework1112/ResisterActivity.java
@@ -1,19 +1,61 @@
 package com.example.sojun_homework1112;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 
 public class ResisterActivity extends AppCompatActivity {
+
+
     ArrayList<String> res_idlist;
     ArrayList<String> res_paslist;
     EditText rid,rpas;
     String ridstr,rpasstr;
+    Button resbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_resister);
+        rid=findViewById(R.id.res_id);
+        rpas=findViewById(R.id.res_pas);
+        resbtn=(Button)findViewById(R.id.res_submitbtn);
+        resbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ridstr=rid.getText().toString();
+                rpasstr=rpas.getText().toString();
+                res_idlist.add(ridstr);
+                res_paslist.add(rpasstr);
+                setStringArrayPref(getApplicationContext(),"id",res_idlist);
+                setStringArrayPref(getApplicationContext(),"pas",res_paslist);
+                Toast.makeText(ResisterActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+    private void setStringArrayPref(Context context, String key, ArrayList<String> values) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        JSONArray a = new JSONArray();
+        for (int i = 0; i < values.size(); i++) {
+            a.put(values.get(i));
+        }
+        if (!values.isEmpty()) {
+            editor.putString(key, a.toString());
+        } else {
+            editor.putString(key, null);
+        }
+        editor.apply();
+    }
+
 }
